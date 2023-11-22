6138c16a4412bfa20651ac9234f721687fd8557a
Brian Erickson <berickson@gmail.com>
2023-11-21 16:09:06 -0800

    Async works, address change doesn't

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include "SparkFun_TMF882X_Library.h"
+#include "async_SparkFun_TMF882X_Library.h"
+//#include "async_qwiic_tmf882x.h"
 
-SparkFun_TMF882X tmf882x;
+Async_SparkFun_TMF882X tmf882x;
 
 #define SAMPLE_TIMEOUT_MS  3000
 
@@ -14,14 +15,36 @@ void measurement_callback(struct tmf882x_msg_meas_results *results) {
 
 void setup() {
 
-    delay(500);
+    delay(3000);
     Serial.begin(921600);
     Serial.println("");
 
-    while(!tmf882x.begin()) {
-        Serial.println("Error - The TMF882X failed to initialize - is the board connected?");
-        delay(1000);
+
+
+
+    for(uint8_t address = 1; address < 128; ++address) {
+      if(!tmf882x.begin(address)) {
+        continue;
+      }
+      Serial.printf("Connected to TMF882X at address %d ", address);
+      break;
+    }   
+    char application_version[80];
+    tmf882x.getApplicationVersion(application_version, 79);
+    Serial.printf("Application version: %s\n", application_version);
+ 
+    Serial.printf("i2c_address: %u\n",tmf882x.getI2CAddress());
+    tmf882x.setI2CAddress_fix(60);
+    while(true) {
+      Serial.println(11);
+      delay(1000);
     }
+    // Serial.println("set the i2address to 32");
+    // Serial.printf("i2c_address: %u\n",tmf882x.getI2CAddress());
+
+    while(true) {
+      delay(1);
+    };
 
     tmf882x.setMeasurementHandler(measurement_callback);
 
@@ -34,12 +57,12 @@ void loop() {
     // currently, one_per_loop is slower, probably because it keeps reinitializing the sensor
     // also, for some reason it is sending some duplicate results
     if (one_per_loop) {
-      tmf882x_msg_meas_results results;
-      const int success = 1, timeout_ms = 100;
-      if(tmf882x.startMeasuring(results, timeout_ms) ==success) { 
-        measurement_callback(&results);
+      tmf882x.async_startMeasuring();
+      while(true) {
+        tmf882x.async_updateMeasuring();
+        Serial.println("processing between measurements");
+        delay(1);
       }
-      delay(30);
     } else {
         tmf882x.startMeasuring(1000, SAMPLE_TIMEOUT_MS);
     } 
